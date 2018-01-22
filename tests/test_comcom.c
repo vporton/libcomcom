e56f4d37976c7d3370fb8a428b8f524b291c1bb5
Victor Porton <porton@narod.ru>
2018-01-22 19:26:23 +0200

    added test suite (does not pass yet)

--- tests/test_comcom.c
+++ tests/test_comcom.c
@@ -19,13 +19,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <check.h>
 #include "libcomcom.h"
 
-/*
- *
- */
+START_TEST(test_short_cat)
+{
+    char buf[3] = "qwe";
+    const char *output;
+    size_t output_len;
+    char *const argv[] = { "\0" };
+    libcomcom_run_command(buf, sizeof(buf),
+                          &output, &output_len,
+                          "cat", argv);
+    /*ck_assert_msg(sizeof(buf) == output_len, "Output buffer size");*/
+    ck_assert_int_eq(sizeof(buf), output_len);
+    ck_assert(!memcmp(output, buf, sizeof(buf)));
+}
+END_TEST
+
+Suite * cat_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Running cat command");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_short_cat);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+
 int main(int argc, char** argv)
 {
-    return (EXIT_SUCCESS);
+    libcomcom_init();
+    libcomcom_set_default_terminate();
+
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = cat_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
