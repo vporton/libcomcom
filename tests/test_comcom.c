fb3c5c8cacf6ccef4981d4d2aa7f7bf41a876935
Victor Porton <porton@narod.ru>
2018-01-22 21:33:01 +0200

    system crashes

--- tests/test_comcom.c
+++ tests/test_comcom.c
@@ -59,6 +59,25 @@ START_TEST(test_long_cat)
 }
 END_TEST
 
+START_TEST(test_long_dd)
+{
+    char buf[1000000];
+    const char *output;
+    size_t output_len;
+    char *const argv[] = { "dd", "bs=100000", "count=10", NULL };
+    int res;
+    for(int i=0; i<sizeof(buf); ++i)
+        buf[i] = i%3;
+    res = libcomcom_run_command(buf, sizeof(buf),
+                                &output, &output_len,
+                                "dd", argv);
+    if(res == -1)
+        ck_abort_msg(strerror(errno));
+    ck_assert_int_eq(sizeof(buf), output_len);
+    ck_assert(!memcmp(output, buf, sizeof(buf)));
+}
+END_TEST
+
 Suite * cat_suite(void)
 {
     Suite *s;
@@ -76,6 +95,22 @@ Suite * cat_suite(void)
     return s;
 }
 
+Suite * dd_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Running dd command");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_long_dd);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -83,11 +118,14 @@ int main(int argc, char** argv)
     libcomcom_set_default_terminate();
 
     int number_failed;
-    Suite *s;
+    Suite *s, *s2;
     SRunner *sr;
 
     s = cat_suite();
+    s2 = dd_suite();
     sr = srunner_create(s);
+    srunner_add_suite(sr, cat_suite());
+    srunner_add_suite(sr, dd_suite ());
 
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
