726f2eac13ab084bd3f7313518f32f199dc53967
Victor Porton <porton@narod.ru>
2018-01-23 21:25:43 +0200

    FIXME comment

--- src/lib.c
+++ src/lib.c
@@ -61,7 +61,7 @@ int libcomcom_init(void)
     if(pipe(self)) return -1;
     if(signal(SIGCHLD, sigchld_handler) == SIG_ERR) return -1;
     return 0;
-}
+
 
 static void clean_pipe(int pipe[2]) {
     if(pipe[0] != -1) {
@@ -95,6 +95,7 @@ static void clean_process_all(my_process_t *process) {
 /* On failure -1 is returned and errno is set. */
 /* TODO: return control on stdout close (don't confuse SIGCHLD of different processes) */
 /* TODO: Support specifying command environment */
+/* FIXME: It seems that EINTR is not always handled! */
 int libcomcom_run_command (const char *input, size_t input_len,
                            const char **output, size_t *output_len,
                            const char *file, char *const argv[])
