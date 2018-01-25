48c01e40ea3509ac4e33d3a1c453cb9a5b033648
Victor Porton <porton@narod.ru>
2018-01-25 03:46:28 +0200

    more verbose tests

--- tests/test_comcom.c
+++ tests/test_comcom.c
@@ -119,7 +119,7 @@ int main(int argc, char** argv)
     sr = srunner_create(s);
     srunner_add_suite(sr, cat_suite());
 
-    srunner_run_all(sr, CK_NORMAL);
+    srunner_run_all(sr, CK_VERBOSE);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
